25cf2d45b78cc574743931c0bf4c0d1321247021
ecorra <eliacorra@gmail.com>
2025-04-20 13:12:09 +0200

    1

---
+++ offseason/workspace/C00/ex05/ft_print_comb.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecorra <eliacorra@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/04 21:20:55 by ecorra            #+#    #+#             */
+/*   Updated: 2025/04/04 22:49:07 by ecorra           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#"APPROCHES: increment and excludes repeats ?
+#"a is always inferior to b and b inferior to c, no repeats
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = 0;
+	b = 1;
+	c = 2;
+	if (a < b && b < c && c =<
+	{
+		write(1, &a, 1)
+		write(1, &b, 1)
+		write(1, &c, 1)
+		write(1, ",", 1)
+		write(1, " ", 1)
+		while (c =< 9)
